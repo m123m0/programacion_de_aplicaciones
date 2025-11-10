8c31d98cbe8d0e6d9779e2bf8c47b962b1f999f4
MURETA1 <m.ureta@duocuc.cl>
2025-11-10 20:42:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sumativa1muz',
+  webDir: 'www'
+};
+
+export default config;
